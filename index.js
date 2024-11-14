f7bba2762fb8c5584a34a00b254bb4b7724a8c3d
localnetwork <diomenikep@gmail.com>
2024-11-14 14:43:11 +0800

    test

--- index.js
+++ index.js
@@ -19,19 +19,20 @@ app.get("/", (req, res) => {
   });
 });
 
+const versions = process.env.VERSIONS.split(",");
+
 // Path to the routes directory
-const routesPath = path.join(__dirname, "./src/routes");
 
-// Read all files in the routes directory
-fs.readdirSync(routesPath).forEach((file) => {
-  const route = require(path.join(routesPath, file));
-  // Assuming each route file exports a router and is used under its own endpoint
-  app.use("/api", route);
-});
-
-// Handle 404 - Route not found
-app.use((req, res, next) => {
-  res.status(404).json({ error: "Not Found" });
+versions.forEach((version) => {
+  const routesPath = path.join(__dirname, `./src/${version}/routes`);
+  if (fs.existsSync(routesPath)) {
+    fs.readdirSync(routesPath).forEach((file) => {
+      const route = require(path.join(routesPath, file));
+      app.use(`/api/${version}`, route);
+    });
+  } else {
+    console.warn(`Routes path not found: ${routesPath}`);
+  }
 });
 
 // Handle other errors (optional)
@@ -39,6 +40,11 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   console.log("err", err);
   res.status(500).json({ error: "Internal Server Error" });
+});
+
+// Handle 404 - Route not found
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Not Found" });
 });
 
 app.listen(process.env.APP_PORT || 1000, () => {
